7c4cc551917ed6edb8463c3f8a235e5d85c7d423
emanuelhardwell <emanuelnoe16@gmail.com>
2021-01-18 13:19:04 -0600

    Operador ternario

---
+++ 02-intro-javascript/intro-javascript/src/base/12-operador-ternario.js
@@ -0,0 +1,19 @@
+/*  */
+/*  */
+const activo = true;
+
+//let mensaje = "";
+
+// if (activo) {
+//   mensaje = "Activo !!!";
+// } else {
+//   mensaje = "Inactivo ---";
+// }
+
+// OPERADOR TERNARIO DE 2 SOLUCIONES
+// let mensaje = !activo ? "Activo !!" : "Inactivo ---";
+
+// OPERADOR TERNARIO DE 1 SOLUCION
+let mensaje = activo && "Activo !!";
+
+console.log(mensaje);
