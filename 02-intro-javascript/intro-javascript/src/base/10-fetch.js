e680edaa0690aba5079d37db65adfe7e26984c12
emanuelhardwell <emanuelnoe16@gmail.com>
2021-01-18 12:29:43 -0600

    Use of Fetch APi with example of giphy API

---
+++ 02-intro-javascript/intro-javascript/src/base/10-fetch.js
@@ -0,0 +1,18 @@
+/*  */
+/*  */
+
+const apiKey = "EMkLUTqDMhIksWGffC42PtHNNshPjgwj";
+
+const peticion = fetch(`http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+
+peticion
+  .then((resp) => resp.json())
+  .then(({ data }) => {
+    /* console.log(data.images.original.url) */
+    const url = data.images.original.url;
+
+    const img = document.createElement("img");
+    img.src = url;
+    document.body.append(img);
+  })
+  .catch(console.warn);
