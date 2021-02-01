5cae5a18ee3ef391ce2af75eb7b539fa0c8c7edf
emanuelhardwell <emanuelnoe16@gmail.com>
2021-01-31 23:57:29 -0600

    Styles applied to the components

--- 04-gif-app/src/GifGrid.js
+++ 04-gif-app/src/GifGrid.js
@@ -1,25 +1,29 @@
 /*  */
-import React, { useState, useEffect } from "react";
+/* import React, { useState, useEffect } from "react"; */
 import { GifGridItem } from "./GifGridItem";
-import { getGifs } from "./helpers/getGif";
+/* import { getGifs } from "./helpers/getGif"; */
+import { useFetchGifs } from "./hooks/useFetchGifs";
 
 export const GifGrid = ({ category }) => {
   /*  */
-  const [image, setImage] = useState([]);
+  /* const [image, setImage] = useState([]); */
   /* con el useEffect defino cuando quiero que se ejecute una funcion  */
-  useEffect(() => {
+  /* useEffect(() => {
     getGifs(category).then((imgs) => setImage(imgs));
-  }, [category]);
+  }, [category]); */
+
+  const { data: image, loading } = useFetchGifs(category);
 
   return (
-    <div>
+    <>
       <h1>{category}</h1>
+      {loading && <p>Loading...</p>}
       {image.map((img) => {
         /* return <li key={img.id}>{img.title} </li>; */
         /* con el {...img} estamos indicando que se van a ir todos los datos y 
          en el siguiente componenete los vamos a DESESTRUCTURAR} */
         return <GifGridItem key={img.id} {...img} />;
       })}
-    </div>
+    </>
   );
 };
--- 04-gif-app/src/GifGridItem.js
+++ 04-gif-app/src/GifGridItem.js
@@ -3,9 +3,9 @@ import React from "react";
 export const GifGridItem = ({ id, title, url }) => {
   /*  */
   return (
-    <div>
+    <div className="card">
+      <img className="img" src={url} alt={title}></img>
       <p>{title}</p>
-      <img src={url} alt={title}></img>
     </div>
   );
 };
