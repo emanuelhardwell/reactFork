2195c3c278b4edf9336153b557e5551888e9fe18
emanuelhardwell <emanuelnoe16@gmail.com>
2021-02-13 10:39:44 -0600

    uso de --- useMemo ---  en HeroList y HeroScreen

--- 07-heroes-app/src/components/heroes/HeroeList.js
+++ 07-heroes-app/src/components/heroes/HeroeList.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { getHeroesByPublisher } from "../../selectors/getHeroesByPublisher";
 import { HeroeCard } from "./HeroeCard";
 
 export const HeroeList = ({ publisher }) => {
   /*  */
-  const heroes = getHeroesByPublisher(publisher);
+
+  const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
+  /* const heroes = getHeroesByPublisher(publisher); */
 
   return (
     <div className="row">
--- 07-heroes-app/src/components/heroes/HeroeScreen.js
+++ 07-heroes-app/src/components/heroes/HeroeScreen.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Redirect, useParams } from "react-router-dom";
 import { getHeroeById } from "../../selectors/getHeroeById";
 
-export const HeroeScreen = () => {
+export const HeroeScreen = ({ history }) => {
   /*  */
   const { heroId } = useParams();
-  const hero = getHeroeById(heroId);
+
+  const hero = useMemo(() => getHeroeById(heroId), [heroId]);
+  /* const hero = getHeroeById(heroId); */
 
   if (!hero) {
     return <Redirect to="/" />;
   }
+
+  const handleReturn = () => {
+    if (history.length <= 2) {
+      history.push("/");
+    } else {
+      history.goBack();
+    }
+  };
 
   const {
     id,
@@ -24,7 +34,7 @@ export const HeroeScreen = () => {
     <div>
       <h1>Hero Screen</h1>
       <div className="row">
-        <div className="col-md-4">
+        <div className="col-md-4 m-1">
           <div className="card">
             <div className="card-body">
               <img
@@ -54,6 +64,9 @@ export const HeroeScreen = () => {
                   Characters: {characters}
                 </li>
               </ul>
+              <button className="btn btn-primary mt-3" onClick={handleReturn}>
+                Return ...
+              </button>
             </div>
           </div>
         </div>
