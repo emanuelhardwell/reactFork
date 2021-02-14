cef28d1e3e6bd1f988c28325ef03855cb036e089
emanuelhardwell <emanuelnoe16@gmail.com>
2021-02-14 16:03:10 -0600

    Uso de useHistory

--- 07-heroes-app/src/components/ui/Navbar.js
+++ 07-heroes-app/src/components/ui/Navbar.js
@@ -1,12 +1,24 @@
 import React, { useContext } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useHistory } from "react-router-dom";
 import { AuthContext } from "../../auth/AuthContext";
+import { types } from "../../types/types";
 
 export const Navbar = () => {
   /*  */
   const {
+    dispatch,
     user: { name },
   } = useContext(AuthContext);
+
+  const history = useHistory();
+
+  const handleLogout = () => {
+    history.replace("/login");
+
+    dispatch({
+      type: types.logout,
+    });
+  };
 
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
@@ -48,14 +60,9 @@ export const Navbar = () => {
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
         <ul className="navbar-nav ms-auto">
           <span className="nav-item nav-link text-warning"> {name} </span>
-          <NavLink
-            activeClassName="active"
-            className="nav-item nav-link"
-            exact
-            to="/login"
-          >
+          <button className="nav-item nav-link btn" onClick={handleLogout}>
             Logout
-          </NavLink>
+          </button>
         </ul>
       </div>
     </nav>
