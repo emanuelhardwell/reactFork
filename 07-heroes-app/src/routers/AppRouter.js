1ab650651a41ea170e37cb79cd129a3312a7ebe7
emanuelhardwell <emanuelnoe16@gmail.com>
2021-02-14 17:03:35 -0600

    Creacion de Rutas privadas

--- 07-heroes-app/src/routers/AppRouter.js
+++ 07-heroes-app/src/routers/AppRouter.js
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { AuthContext } from "../auth/AuthContext";
 import { LoginScreen } from "../components/login/LoginScreen";
 import { MarvelScreen } from "../components/marvel/MarvelScreen";
 import { Navbar } from "../components/ui/Navbar";
 import { DashboardRoutes } from "./DashboardRoutes";
+import { PrivateRoute } from "./PrivateRoute";
 
 export const AppRouter = () => {
+  /*  */
+
+  const { user } = useContext(AuthContext);
+
   return (
     <Router>
       <div>
-        {/*  */}
-
         <Switch>
           <Route exact path="/login" component={LoginScreen} />
 
-          <Route path="/" component={DashboardRoutes} />
+          <PrivateRoute
+            path="/"
+            component={DashboardRoutes}
+            isAuthenticated={user.logged}
+          />
         </Switch>
       </div>
     </Router>
