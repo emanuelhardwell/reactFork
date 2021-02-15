c24425eb580f9984708a33e7d66d998d5a233f96
emanuelhardwell <emanuelnoe16@gmail.com>
2021-02-14 22:44:10 -0600

    Recordar la ultima pagina visitada

--- 07-heroes-app/src/routers/PrivateRoute.js
+++ 07-heroes-app/src/routers/PrivateRoute.js
@@ -8,6 +8,9 @@ export const PrivateRoute = ({
   component: Component,
   ...rest
 }) => {
+  /* console.log(rest.location.pathname); */
+  localStorage.setItem("lastPath", rest.location.pathname);
+
   return (
     <Route
       {...rest}
